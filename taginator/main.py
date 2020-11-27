4de4bd48be591ea756f2b3d2f3b1955f6e3b5669
z-mrozu <43796046+z-mrozu@users.noreply.github.com>
2020-11-27 19:25:12 +0100

    taginotorowanie biednych plików .txt z katalogu

--- taginator/main.py
+++ taginator/main.py
@@ -1,3 +1,21 @@
+import os
+
+text=[]
+
+def import_txt():
+    print("Importing files...")
+    filelist = os.listdir(os.getcwd())
+    for file in filelist:
+        if file.endswith(".txt"):
+            print(file) #filename
+            try:
+                with open(file, 'r') as f:
+                    contents = f.read()
+                    text.append(contents)
+            except:
+                pass
+    print("Done!")
+
 def main():
     print("Hello")
 
