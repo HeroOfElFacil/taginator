83cbb7ce8a01ccba3da374db2c51ad44b81752d5
niladrem <aurbala@gmail.com>
2020-11-27 18:33:45 +0100

    change init

--- taginator/__init__.py
+++ taginator/__init__.py
@@ -1,10 +1,1 @@
-from distutils.version import LooseVersion
-
-from pkg_resources import get_distribution
-
-__all__ = ["PACKAGE", "__version__"]
-
-
-PACKAGE = get_distribution("taginator")
-
-__version__ = LooseVersion(PACKAGE.version)
+__version__ = "0.1"
