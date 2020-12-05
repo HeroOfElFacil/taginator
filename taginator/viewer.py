d150532ca65df0d714091542d900f4b9d6de6dfd
Olson <43792881+AleksanderZawisza@users.noreply.github.com>
2020-12-05 11:20:14 +0100

    Update viewer.py

--- taginator/viewer.py
+++ taginator/viewer.py
@@ -6,5 +6,19 @@ class Viewer():
             for key, value in note.extracted_values.items():
                 print("\t", key, ":")
                 print("\t\t", ",".join(value))
+       
+    def show_file(self, files, file_name):
+        if file_name == "all":
+            for name, note in files.items():
+                print("\n", name)
+                print("\n", note.text)
+        else:
+            try:
+                for name, note in files.items():
+                    if os.path.splitext(name)[0] == file_name:
+                        print("\n", name)
+                        print("\n", note.text)
+            except:
+                print("Note not found")
 
 
