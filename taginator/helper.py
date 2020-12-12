29acd0c81db4bf8f60f5e23c7554db70a11b2561
Kacper Staron <staronkacper99@gmail.com>
2020-12-12 18:12:48 +0100

    updating help for command show

--- taginator/helper.py
+++ taginator/helper.py
@@ -7,7 +7,7 @@ class Helper():
                   "Both local and global file paths work. To import from current folder leave empty.\n")
         elif command == "help show":
             print("\n\t\'show\' first prompts the user for a file name of an imported note and then shows its content and all automatically generated tags. "
-                  "Input \'all\' to show all currently imported notes.\n")
+                  "Input \'all\' to show all currently imported notes.\n Shown notes can be inspected for any occurence of the specific word. Te enter an inspection mode type [y] when prompted. To exit inspection mode type [n] when prompted. \n")
         elif command == "help list":
             print("\n\t\'list\' lists all currently imported files and their automatically generated tags.\n")
         elif command == "help exit":
